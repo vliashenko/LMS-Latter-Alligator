2a53c57762c18a3fc6d8f7d8b86bcc0997583d70
Liashenko <greedisdood1@gmail.com>
2024-08-05 16:50:37 +0300

    learn page layout added

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,15 @@
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+
 export default function Learn() {
   return (
-    <div>page</div>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+        <StickyWrapper>
+            my sticky wrapper
+        </StickyWrapper>
+        <FeedWrapper>
+            My feed wrapper 
+        </FeedWrapper>
+    </div>
   )
 }
---
+++ components/sticky-wrapper.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function StickyWrapper({ children }: Props) {
+  return (
+    <div className="hidden lg:block w-[368px] sticky self-end bottom-6">
+      <div className="min-h-[calc(100vh-48px)] sticky top-6 flex flex-col gap-y-5">{children}</div>
+    </div>
+  );
+}
