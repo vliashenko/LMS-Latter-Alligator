d04bcb1b77c33245a0704149feaed1fb023f4709
Liashenko <greedisdood1@gmail.com>
2024-08-07 15:56:22 +0300

    units, lessons and challenges added

--- db/queries.ts
+++ db/queries.ts
@@ -2,15 +2,15 @@ import { cache } from "react";
 import db from "@/db/drizzle";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
-import { courses, userProgress } from "@/db//schema";
+import { courses, units, userProgress } from "@/db//schema";
 
-export const getCourses = cache(async() => {
+export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany();
 
     return data;
 })
 
-export const getUserProgress = cache(async() => {
+export const getUserProgress = cache(async () => {
     const { userId } = await auth();
 
     if (!userId) {
@@ -27,10 +27,47 @@ export const getUserProgress = cache(async() => {
     return data;
 })
 
-export const getCourseById = cache(async(courseId: number) => {
+export const getCourseById = cache(async (courseId: number) => {
     const data = await db.query.courses.findFirst({
         where: eq(courses.id, courseId),
     })
 
     return data;
 })
+
+export const getUnits = cache(async () => {
+    const userProgress = await getUserProgress();
+
+    if (!userProgress?.activeCourseId) {
+        return [];
+    }
+
+    const data = await db.query.units.findMany({
+        where: eq(units.courseId, userProgress.activeCourseId),
+        with: {
+            lessons: {
+                with: {
+                    challenges: {
+                        with: {
+                            challengeProgress: true,
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    const normalizedData = data.map((unit) => {
+        const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+            const allCompletedChallenges = lesson.challenges.every((challenge) => {
+                return challenge.challengeProgress && challenge.challengeProgress.length > 0 && challenge.challengeProgress.every((progress) => progress.completed)
+            });
+
+            return { ...lesson, completed: allCompletedChallenges };
+        });
+
+        return { ...unit, lessons: lessonsWithCompletedStatus };
+    });
+
+    return normalizedData;
+});
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,6 +14,12 @@ const main = async () => {
 
         await db.delete(schema.courses);
         await db.delete(schema.userProgress);
+        await db.delete(schema.units);
+        await db.delete(schema.lessons);
+        await db.delete(schema.challenges);
+        await db.delete(schema.challengeOptions);
+        await db.delete(schema.challengeProgress);
+
         await db.insert(schema.courses).values([
             {
                 id: 1,
@@ -40,6 +46,70 @@ const main = async () => {
                 title: 'Upper Intermediate',
                 imageSrc: '/crocodile.png'
             },
+        ]);
+
+        await db.insert(schema.units).values([
+            {
+                id: 1,
+                courseId: 1,
+                title: 'Unit 1',
+                description: 'Hello, what’s your name?',
+                order: 1,
+            }
+        ])
+
+        await db.insert(schema.lessons).values([
+            {
+                id: 1,
+                unitId: 1,
+                order: 1,
+                title: 'Learning the Alphabet',
+            },
+        ])
+
+        await db.insert(schema.challenges).values([
+            {
+                id: 1,
+                lessonId: 1,
+                type: 'SELECT',
+                order: 1,
+                question: 'Назва якої тварини починається на останню літеру англійської абетки?'
+            },
+        ])
+
+        await db.insert(schema.challengeOptions).values([
+            {
+               id: 1,
+               challengeId: 1,
+               imageSrc: '/zebra.png',
+               correctOption: true,
+               text: 'Zebra',
+               audioSrc: '/zebra.mp3',
+            },
+            {
+                id: 2,
+                challengeId: 1,
+                imageSrc: '/bear.png',
+                correctOption: false,
+                text: 'Bear',
+                audioSrc: '/bear.mp3',
+             },
+             {
+                id: 3,
+                challengeId: 1,
+                imageSrc: '/dog.png',
+                correctOption: false,
+                text: 'Dog',
+                audioSrc: '/dog.mp3',
+             },
+             {
+                id: 4,
+                challengeId: 1,
+                imageSrc: '/crocodile.png',
+                correctOption: false,
+                text: 'Crocodile',
+                audioSrc: '/crocodile.mp3',
+             },
         ])
 
         console.log('Seeding is finished');
