619159879d50df15589ca964aecb52b0335295e2
Liashenko <greedisdood1@gmail.com>
2024-08-08 11:54:45 +0300

    repository updated

--- db/queries.ts
+++ db/queries.ts
@@ -162,3 +162,22 @@ export const getLesson = cache(async (id?: number) => {
 
     return { ...data, challenges: normalizedChallenges };
 })
+
+export const getLessonPercentage = cache(async () => {
+    const courseProgress = await getCourseProgress();
+
+    if (!courseProgress?.activeLessonId) {
+        return 0
+    }
+
+    const lesson = await getLesson(courseProgress.activeLessonId);
+
+    if (!lesson) {
+        return 0
+    }
+
+    const completedChallenges = lesson.challenges.filter((challenge) => challenge.completed);
+    const percentage = Math.round((completedChallenges.length / lesson.challenges.length) * 100)
+
+    return percentage;
+})
