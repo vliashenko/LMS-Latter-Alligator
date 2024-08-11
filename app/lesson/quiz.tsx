145bebdc1003506e6e6a4d058f7f80d876aeac35
Liashenko <greedisdood1@gmail.com>
2024-08-11 14:25:12 +0300

    server actions updated

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -41,14 +41,14 @@ export default function Quiz({
   const [selectedOption, setSelectedOption] = useState<number>();
   const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
 
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
+
   const onSelect = (id: number) => {
     if (status !== "none") return;
 
     setSelectedOption(id);
   };
-
-  const challenge = challenges[activeIndex];
-  const options = challenge?.challengeOptions ?? [];
 
   const onNext = () => {
     setActiveIndex((current) => current + 1);
