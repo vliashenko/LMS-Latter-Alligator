c80199a691bda3275bef6bbab57016df19125035
Liashenko <greedisdood1@gmail.com>
2024-08-17 17:22:24 +0300

    admin route fixed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,12 @@
+import { redirect } from "next/navigation";
+import { UserService } from "@/services/users";
 import App from "./app";
 
 export default async function AdminPage() {
+  const admin = await UserService.isAdmin();
+
+  if (!admin) {
+    redirect("/");
+  }
   return <App />;
 }
