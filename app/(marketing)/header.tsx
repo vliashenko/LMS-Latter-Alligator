3f7c20ecad983e82d3c73d87749f85be53c6a06a
Liashenko <greedisdood1@gmail.com>
2024-08-19 19:35:20 +0300

    header and footer for marketing page added

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
-        
+        header
       </div>
     </header>
   );
