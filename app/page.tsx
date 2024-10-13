aa41315c0fb1149212e6f2b278cd77dfd848ed33
Liashenko <greedisdood1@gmail.com>
2024-10-13 19:04:14 +0300

    home page added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <Button variant={"dangerOutline"}>Click me</Button>
-  );
+  return <div></div>;
 }
